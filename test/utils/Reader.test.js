8c81d2c2f440512999a303a71b05d02ccd8f87d0
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-05-16 14:05:08 -0500

    Creando archivo de prueba con la ista de explorers y modificando la prueba unitaria de la clase Reader

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,9 @@
+const Reader = require("./../../lib/utils/Reader");
+
+describe("Test de clase Reader", () => {
+    test("Obtener la lista de todos los explorers", () => {
+        
+        const ExplorersRead = Reader.readJsonFile("./test/utils/Prueba.json");
+        expect(ExplorersRead.length).toBe(5);
+    })
+})
