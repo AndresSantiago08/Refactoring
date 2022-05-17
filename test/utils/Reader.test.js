22ae8160e9c3848b8d63224b3c4404ca99bf381d
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-05-16 23:29:44 -0500

    Pasando pruebas locales

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -3,7 +3,7 @@ const Reader = require("./../../lib/utils/Reader");
 describe("Test de clase Reader", () => {
     test("Obtener la lista de todos los explorers", () => {
         
-        const ExplorersRead = Reader.readJsonFile("https://github.com/AndresSantiago08/Refactoring/blob/master/test/utils/prueba.json");
+        const ExplorersRead = Reader.readJsonFile("./test/utils/prueba.json");
         expect(ExplorersRead.length).toBe(5);
     })
 })
