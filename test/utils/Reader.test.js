775fe224142ea235a03e4c5f8e4e028c3c80e316
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-05-16 21:27:56 -0500

    Modificando ruta del archivo prueba.json

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -3,7 +3,7 @@ const Reader = require("./../../lib/utils/Reader");
 describe("Test de clase Reader", () => {
     test("Obtener la lista de todos los explorers", () => {
         
-        const ExplorersRead = Reader.readJsonFile("./test/utils/Prueba.json");
+        const ExplorersRead = Reader.readJsonFile("Prueba.json");
         expect(ExplorersRead.length).toBe(5);
     })
 })
